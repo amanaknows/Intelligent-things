ceeddebf3a7bf3bf8288f6ef29c1c5f32080ef32
amanaknows <33403219+amanaknows@users.noreply.github.com>
2024-08-06 01:06:04 -0400

    Update quantative-easing-thing.py
    Sourced from Goldman-Sachs

--- Quantative-easing-thing.py
+++ Quantative-easing-thing.py
@@ -1,4 +1,4 @@
-
+//thanks goldman-sachs! just what we needed c:
 
 <a href="https://app.commanddash.io/agent?github=https://github.com/goldmansachs/gs-quant"><img src="https://img.shields.io/badge/AI-Code%20Assist-EB9FDA"></a>
 
